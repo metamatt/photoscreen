7384d0e24daef678a8f3b02ca1ef9e5e60f711d7
Matt Ginzton <matt@ginzton.net>
2013-01-13 22:04:53 -0800

    Handle JPG and PNG directly with PIL, and pipe all other files through dcraw (assuming it's on the path). Also, add exception handling enough that we nicely ignore files that either dcraw or PIL refuse to load.
    This is enough that I can process a directory containing .jpg, .xmp
    and .nef files (and we create thumbnails for the .jpg and .nef and
    ignore the .xmp).

--- add_photos.py
+++ add_photos.py
@@ -6,7 +6,9 @@
 import hashlib
 import os
 import StringIO
+import subprocess
 import sys
+import traceback
 # public libs
 import Image
 # private libs
@@ -17,32 +19,51 @@ db = database.Database()
 size = (400, 400)
 count = 0
 
+
+class NotAnImageError(Exception):
+   pass
+
 for photo_file_path in sys.argv[1:]:
-   # get full path
-   path = os.path.realpath(photo_file_path)
+   try: # wrap loop in exception handler so if one file fails, we keep going
+      # get full path
+      path = os.path.realpath(photo_file_path)
 
-   # get thumbnail
-   im = Image.open(photo_file_path)
-   im.thumbnail(size, Image.ANTIALIAS)
-   #thumb = buffer(im.tostring('jpeg'))
-   stringbuf = StringIO.StringIO()
-   im.save(stringbuf, 'JPEG')
-   thumb = buffer(stringbuf.getvalue())
+      # get thumbnail: read image and resize to 400x400
+      # hack for raw files: read JPEG files directly with PIL; all others get piped through dcraw
+      extension = os.path.splitext(photo_file_path)[1][1:].upper()
+      try:
+         if extension in ('JPG', 'JPEG', 'PNG'):
+            im = Image.open(photo_file_path)
+         else:
+            bytes = subprocess.check_output(['dcraw', '-h', '-c', photo_file_path], stderr = subprocess.STDOUT)
+            im = Image.open(StringIO.StringIO(bytes))
+      except subprocess.CalledProcessError, IOError:
+         raise NotAnImageError
+      im.thumbnail(size, Image.ANTIALIAS)
+      stringbuf = StringIO.StringIO()
+      im.save(stringbuf, 'JPEG')
+      thumb = buffer(stringbuf.getvalue())
 
-   # get content hash
-   with open(photo_file_path, 'rb') as contents:
-      digester = hashlib.sha1()
-      while True:
-         buf = contents.read(4096)
-         if not buf:
-            break
-         digester.update(buf)
-      digest = digester.hexdigest()
+      # get content hash
+      with open(photo_file_path, 'rb') as contents:
+         digester = hashlib.sha1()
+         while True:
+            buf = contents.read(4096)
+            if not buf:
+               break
+            digester.update(buf)
+         digest = digester.hexdigest()
 
-   # add to database
-   print 'adding %s (%d KB)' % (photo_file_path, len(thumb) / 1024)
-   db.add_photo(path, thumb, digest)
-   count += 1
+      # add to database
+      print 'adding %s (%d KB)' % (photo_file_path, len(thumb) / 1024)
+      db.add_photo(path, thumb, digest)
+      count += 1
+   except NotAnImageError:
+      print 'skipping %s (not an image file)' % photo_file_path
+   except KeyboardInterrupt:
+      raise
+   except: # keep going on other exceptions
+      traceback.print_exc(0)
 
 db.commit()
 print 'added %d photos' % count
